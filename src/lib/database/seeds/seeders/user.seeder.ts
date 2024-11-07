85cda6403e6bf968dbe464aa3e1515cb75708a31
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-11-07 09:11:29 -0300

    Fix: seeds

--- src/lib/database/seeds/seeders/user.seeder.ts
+++ src/lib/database/seeds/seeders/user.seeder.ts
@@ -15,11 +15,11 @@ export default class UserSeeder implements Seeder {
     const userRepository = dataSource.getRepository(User);
 
     const newUser = userRepository.create({
-      user_name: 'admin',
-      user_email: 'user@example.com',
-      hashed_password: await createHashedPassword('password123'),
-      phone_number: '(11) 11111-1111',
-      date_of_birth: '2003-12-09',
+      user_name: 'Marcello Samura',
+      user_email: 'marcellosamura@gmail.com',
+      hashed_password: await createHashedPassword('senha123'),
+      phone_number: '(19) 98950-7221',
+      date_of_birth: '1978-04-17',
     });
 
     await userRepository.save(newUser);
--- src/modules/auth/dtos/login.dto.ts
+++ src/modules/auth/dtos/login.dto.ts
@@ -17,13 +17,13 @@ export type LoginPayload = z.infer<typeof loginSchema>;
 export class LoginDTO extends createZodDto(loginSchema) {
   @ApiProperty({
     description: 'The email of the user',
-    example: 'user@example.com',
+    example: 'marcellosamura@gmail.com',
   })
   user_email: string;
 
   @ApiProperty({
     description: 'The password of the user',
-    example: 'password123',
+    example: 'senha123',
   })
   password: string;
 }
